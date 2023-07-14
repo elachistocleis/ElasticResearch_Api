c62a74c30e647c2c034f247d8e4320cd0a1fe6f5
elachistocleis <30549938+elachistocleis@users.noreply.github.com>
2023-07-14 14:08:49 -0500

    Update elastic-test-local.js

--- elastic-test-local.js
+++ elastic-test-local.js
@@ -46,7 +46,7 @@ async function run() {
         body: {
           title: item.title,
           author: item.author,
-          description: item.description
+          description: item.description,
         }
       });
     }
