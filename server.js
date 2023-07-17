f2f0944211b087e755a613320b82f0f5a25d2a39
elachistocleis <30549938+elachistocleis@users.noreply.github.com>
2023-07-17 16:43:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/search', async (req, res) => {
 
     try {
         const response = await client.search({
-            index: 'google',
+            index:'google',
             body: {
               query: {
                 match: { [field]: query }
